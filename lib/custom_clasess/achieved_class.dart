8a5415d4f6163e34ba4cefd5f343540627812627
iemtiyaz.tharwat <iemtiyaztharwat@gmail.com>
2025-09-11 10:09:13 +0300

    Achieved Tasks is finally done

--- lib/custom_clasess/achieved_class.dart
+++ lib/custom_clasess/achieved_class.dart
@@ -17,41 +17,47 @@ class AchievedTasksProgress extends StatelessWidget {
     int percentage = (progress * 100).toInt();
 
     return Container(
-      height: 74,
+      height: 72,
       width: double.infinity,
       decoration: BoxDecoration(color: Color(0xff282828), borderRadius: BorderRadius.circular(20),),
-      child: ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 16),
-        title: const Text(
-          'Achieved Tasks',
-          style: TextStyle(
-            color: Color(0xffFFFCFC),
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-            fontFamily: "Poppins",
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+            'Achieved Tasks',
+            style: TextStyle(
+              color: Color(0xffFFFCFC),
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              fontFamily: "Poppins",
+            ),
+                    ), 
+                    Text(
+            '$completedTasks out of $totalTasks Done',
+            style: const TextStyle(
+              color: Color(0xffC6C6C6),
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              fontFamily: "Poppins",
+            ),
+                    ),
+              ],
+            ),
           ),
-        ),
-        subtitle: Text(
-          '$completedTasks out of $totalTasks Done',
-          style: const TextStyle(
-            color: Color(0xffC6C6C6),
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            fontFamily: "Poppins",
-          ),
-        ),
-        trailing: SizedBox(
-          width: 46,
-          height: 46,
-          child: CircularPercentIndicator(radius: 26, lineWidth: 5.0,
+          SizedBox(width: 140,),
+          CircularPercentIndicator(radius: 26, lineWidth: 5.0,
               percent: progress,
                animation: true, animationDuration: 1200, circularStrokeCap: CircularStrokeCap.round,
               center: Text('$percentage%', style: TextStyle( color: Colors.white,
               fontSize: 14,
               fontWeight: FontWeight.w500,),),
               progressColor: Color(0xff15B86C),),
-        ),
-      ),
+        ],
+      )
     );
   }
 }
